1a47468822981bb6ec29eb7f048784f826b1b369
chris <104324755+cjurgens17@users.noreply.github.com>
2023-06-25 20:21:44 -0400

    Made dropdown fucntionality for navbar when screen gets smaller, just need to fix bug where it is persisting if clicked when the screen goes back to a larger size.

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeatherModule } from 'angular-feather';
-import { Menu, ShoppingCart, Coffee} from 'angular-feather/icons';
+import { Menu, ShoppingCart, Coffee, X} from 'angular-feather/icons';
 
 
 const icons = {
     Menu,
     ShoppingCart,
-    Coffee
+    Coffee,
+    X
 }
 
 @NgModule({
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  options: boolean = false;
 
+  public showOptions(): void{
+    const getShow = document.getElementById('burgerOptions') as HTMLElement;
+    const menuBar = document.getElementById('menubar') as HTMLElement;
+
+    this.options = !this.options;
+
+    if (this.options) {
+      getShow.classList.add('show');
+      menuBar.style.display = 'none';
+    }else {
+      getShow.classList.remove('show');
+    }
+
+    this.options = !this.options;
+  }
+
+  public backToBurger(): void {
+    const menuBar = document.getElementById('menubar') as HTMLElement;
+    const getShow = document.getElementById('burgerOptions') as HTMLElement;
+    // const screenWidth = window.innerWidth;
+
+    // if(screenWidth < 900){
+    //   menuBar.style.display = 'revert';
+    // }else {
+    //   menuBar.style.display = 'none';
+    // }
+    menuBar.style.display = 'revert';
+    getShow.classList.remove('show');
+  }
 }
