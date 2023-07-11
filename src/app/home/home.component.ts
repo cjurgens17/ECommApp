a844f30f3139be88124699a2ad2a63e8622383ca
chris <104324755+cjurgens17@users.noreply.github.com>
2023-07-11 19:55:52 -0400

    Ended up making own custom carousel, will finish and polish tomorrow. Also still need to import postcss loader, or decide to uninstall library

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,13 +10,41 @@ import { Subject, takeUntil } from 'rxjs';
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
+carouselIndex: number = 0;
 advertisedProducts!: Product[];
 private ngUnSubscribe = new Subject<void>();
+carouselImages: string[] = [
+  'https://images.unsplash.com/photo-1536914629078-6fda32a00cd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29mZmVlJTIwcG93ZXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+  'https://media.istockphoto.com/id/627331730/photo/successful-middle-age-businessman.webp?b=1&s=170667a&w=0&k=20&c=5irIXSfOtmUQrQLH18gLiUP7lgsghRD_OvgjdGrVLAQ=',
+  'https://media.istockphoto.com/id/1414977595/photo/woman-stir-coffee-with-coffee-spoon-coffee-cup-hot-drink-in-a-mug-beige-and-marble-background.webp?b=1&s=170667a&w=0&k=20&c=1N2xzyo7raTnkLarL2Rzw_JbZ7UN8L6q1sBSIy_OjLY='
+];
 
 constructor(private productService: ProductsService) {}
 
 scrollToTop(){
   window.scroll(0,0);
+}
+
+moveLeft(){
+  if(this.carouselIndex === this.carouselImages.length - 1){
+    this.carouselIndex = 0;
+  }else{
+    this.carouselIndex++;
+  }
+}
+
+moveRight(){
+  console.log(this.carouselIndex);
+  if(this.carouselIndex === 0){
+    this.carouselIndex = this.carouselImages.length-1;
+  }else{
+    this.carouselIndex--;
+  }
+}
+
+updateCarouselBackground(){
+  let carouselItem = document.getElementById('carousel') as HTMLElement;
+  carouselItem.style.backgroundImage = "url(" + this.carouselImages[this.carouselIndex] + ")";
 }
 
   ////Lifecycle Hooks
