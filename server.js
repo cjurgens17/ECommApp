2e7a413dc87cc1b8db485ea7f03662d3fb385dd8
chris <104324755+cjurgens17@users.noreply.github.com>
2023-07-22 18:27:34 -0400

    getting ready for production

--- server.js
+++ server.js
@@ -1,51 +1,66 @@
 require('dotenv').config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// const bodyParser = require('body-parser');
+// app.use(cors());
+// app.use(express.static('public'));
+// app.use(bodyParser.json());
+
+// const MY_DOMAIN = 'http://localhost:4200';
+
+// app.get('/', (req, res) => {
+//   res.send('Welcome to My Application'); // Send the welcome text to the view
+//   console.log("the server is live!");
+// });
+
+// app.post('/create-checkout-session', async (req,res) => {
+// try{
+//   console.log(req.body);
+//   const cartProducts = req.body.products;
+
+//   const lineItems = await cartProducts.map((product) => ({
+//     price_data: {
+//       currency: 'usd',
+//       product_data: {
+//         name: product.name,
+//         images: [product.image],
+//       },
+//       unit_amount: parseInt(product.price * 100), //converting to cents for stripe api
+//     },
+//     quantity: product.quantity,
+//   }));
+
+//   const session = await stripe.checkout.sessions.create({
+//     line_items: lineItems,
+//     mode: 'payment',
+//     success_url: `${MY_DOMAIN}/success`,
+//     cancel_url: `${MY_DOMAIN}/cancel`
+//   });
+
+//   console.log('Stripe Response:', session);
+//   res.json({url: session.url})
+// } catch (e) {
+//   console.error("stripe Post error: ", e);
+//   res.status(500).json({ error: 'Error occurred while processing the request.' });
+// }
+// });
+
+// app.listen(4242, () => console.log('Running in port 4242'));
+
+
+//connect to heroku
 const express = require('express');
-const cors = require('cors');
+const path = require('path');
+
 const app = express();
-const bodyParser = require('body-parser');
-app.use(cors());
-app.use(express.static('public'));
-app.use(bodyParser.json());
+app.use(express.static('./dist/ecommapplication'));
 
-const MY_DOMAIN = 'http://localhost:4200';
+app.get('/*', (req,res) =>
+res.sendFile(path.join(__dirname, 'dist/ecommapplication', 'index.html')),
+);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to My Application'); // Send the welcome text to the view
-  console.log("the server is live!");
-});
+app.listen(process.env.PORT || 8080);
 
-app.post('/create-checkout-session', async (req,res) => {
-try{
-  console.log(req.body);
-  const cartProducts = req.body.products;
-
-  const lineItems = await cartProducts.map((product) => ({
-    price_data: {
-      currency: 'usd',
-      product_data: {
-        name: product.name,
-        images: [product.image],
-      },
-      unit_amount: parseInt(product.price * 100), //converting to cents for stripe api
-    },
-    quantity: product.quantity,
-  }));
-
-  const session = await stripe.checkout.sessions.create({
-    line_items: lineItems,
-    mode: 'payment',
-    success_url: `${MY_DOMAIN}/success`,
-    cancel_url: `${MY_DOMAIN}/cancel`
-  });
-
-  console.log('Stripe Response:', session);
-  res.json({url: session.url})
-} catch (e) {
-  console.error("stripe Post error: ", e);
-  res.status(500).json({ error: 'Error occurred while processing the request.' });
-}
-});
-
-app.listen(4242, () => console.log('Running in port 4242'));
 
