e01cd81f5b94e1bdd680b1d9a0122d3c5ef38d20
chris <104324755+cjurgens17@users.noreply.github.com>
2024-01-13 10:37:00 -0500

    updating environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,9 @@ export const environment = {
 
   production: false,
   withCredentials: true,
-  baseUrl: "https://ecomm-app-server.vercel.app/home",
+  baseUrl: "https://ecomm-app-server.vercel.app",
   headers: {
-    'content-type' : 'application/json',
-    'Access-Control-Allow-Origin': 'https://ecommerce-app-three-tau.vercel.app/home'
+    'content-type' : 'application/json'
   }
 };
 
