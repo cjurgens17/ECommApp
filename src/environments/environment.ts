b46142ff7f9536814d1584dca5d3ded68f756640
Chris <104324755+cjurgens17@users.noreply.github.com>
2024-01-14 15:47:52 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
 
   production: false,
   withCredentials: true,
-  baseUrl: "https://ecomm-app-server.vercel.app",
+  baseUrl: "https://coffee-brothers-server-578023311cb8.herokuapp.com",
   headers: {
     'content-type' : 'application/json'
   }
